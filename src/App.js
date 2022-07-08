2c105f15006ab0deaa2bd5b01e228e49ddc92a1c
RThemian <62709684+RThemian@users.noreply.github.com>
2022-07-08 12:12:24 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-
+import logo1 from './logo 1.png'
 import './App.css';
+import TodoList from './components/TodoList.js';
+
 
 function App() {
   return (
     <>
     
     <div className ="banner">
-
-        <h1>Super To Do</h1>
+    <header>
+      <div className = "col-xs-3">
+      
+      <img src={logo1} alt ="logo1"/>
+      </div>
+      <div className ="col-xs-3">
+      <h1 className="headline">Super To Do</h1>
+       
+      </div>
+      </header>  
       
     </div>
+   
+     <TodoList/>
+   
     </>
   );
 }
