fbc472ef7272d691d396aa8d60488dbfc7a315e0
RThemian <62709684+RThemian@users.noreply.github.com>
2022-07-08 11:07:28 -0600

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Todo APP</h1>
+        <h1>Todo APP -- first change</h1>
       </header>
     </div>
   );
